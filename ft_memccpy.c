534d3e4db1e447c8dabe30a8b87edbe7e4c860e9
Daf <daphenie.felissaint@gmail.com>
2018-12-18 00:04:48 +0100

    modif

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -1,7 +1,5 @@
 #include	"libft.h"
 
-#include <stdio.h>
-
 void				*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
 	size_t					i;
@@ -19,11 +17,4 @@ void				*ft_memccpy(void *dest, const void *src, int c, size_t n)
 		i++;
 	}
 	return (NULL);
-}
-
-int	main(int ac, char **av)
-{
-	printf("%p\n", ft_memccpy(av[2], av[1], 'l', 5));
-	printf("%p", memccpy(av[2], av[1], 'l', 5));
-	return (0);
 }
--- libft.h
+++ libft.h
@@ -20,7 +20,7 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_atoi(const char *nptr);
 int		ft_strcmp(const char *s1, const char *s2);
-int		ft_memcmp(const void *s1, const void *s2; size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_strequ(char const *s1, char const *s2);
 int		ft_strnequ(char const *s1, char const *s2, size_t n);
